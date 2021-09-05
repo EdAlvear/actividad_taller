d9c4faf4fc1ec75d3c972db3e5d41d93ef13fc14
Eduardo Alvear <ealvear0312@live.com>
2021-09-04 23:49:10 -0500

    Ejercicio 2

--- ejercicios_taller.py
+++ ejercicios_taller.py
@@ -5,11 +5,12 @@ Created on Sat Sep  4 23:40:54 2021
 @author: Eduardo Alvear
 """
 
-# Ejercicio 1
+# Ejercicio 1.1 
 def ejercicio1():
     y = ( ( 5 + 2 - 5)**2 * 5 + 8 / 2 - 30) / 2 * 5 - 3
     return y
 
+# Ejercicio 1.2 
 def ejercicio2():
     z = 5
     n = 3
@@ -17,9 +18,15 @@ def ejercicio2():
     y = (((z + 2 - n)**2 * m + 8 / 2 - 30) / 2 * 5 - 3 )**5 + 15 * 3 - 9 / 3
     return y
 
+# Ejercicio 1.3
 def ejercicio3():
     z = 5
     n = ((8+2-4)**2 * 5 + 8 + 7 / 2 - 30* 5 ) / 2 * 5 - 3;
     m = z**2 * 3 + n;
     y = ((((z+2-n)**2 * m+8/2 - 30) / 2 * 5 - 3)**5 + 15 * 3 - 9 / 3)**2 - 5 / 4
     return y
+
+# Ejercicio 2
+
+def punto1(p, v, t):
+    return (p * v) / (0.37 * (t + 460))
